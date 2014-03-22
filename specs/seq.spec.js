3cd82f1524c665d13ef64be1b2eeef1330efe0bb
Itay Maman <imaman@google.com>
2014-03-22 16:43:24 +0200

    support three-fun seq, with only one arg

--- specs/seq.spec.js
+++ specs/seq.spec.js
@@ -22,16 +22,33 @@ describe('Seq', function() {
   it('translates a sequence of two functions', function() {
     console.log('\n------------------------------------------\n');
     var seq = new Seq([
-      new Fun(function(v1, v2, next) {
-        next(null, v1 + v2) }),
+      new Fun(function(v1, next) {
+        next(null, v1 + 4) }),
       new Fun(function(v, next) {
         next(null, v * v) })
     ]);
     var wrapped = seq.wrap();
     var args;
-    wrapped(2, 4, function() {
+    wrapped(null, 2, function() {
       args = Array.prototype.slice.call(arguments, 0);
     });
     expect(args).toEqual([null, 36]);
+  });
+  it('translates a sequence of three functions', function() {
+    console.log('\n------------------------------------------\n');
+    var seq = new Seq([
+      new Fun(function(v, next) {
+        next(null, v * 2) }),
+      new Fun(function(v, next) {
+        next(null, v * 3) }),
+      new Fun(function(v, next) {
+        next(null, v * 5) })
+    ]);
+    var wrapped = seq.wrap();
+    var args;
+    wrapped(null, 7, function() {
+      args = Array.prototype.slice.call(arguments, 0);
+    });
+    expect(args).toEqual([null, 210]);
   });
 });
