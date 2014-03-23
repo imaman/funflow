0da09c840f6242b42284f81222f364b95196f62d
Itay Maman <imaman@google.com>
2014-03-23 23:58:24 +0200

    first few tests for conc

---
+++ specs/conc.spec.js
@@ -0,0 +1,23 @@
+var Conc = require('../lib/conc');
+var Fun = require('../lib/fun');
+
+describe('Conc', function() {
+  it('takes a single object argument', function() {
+    new Conc({});
+  });
+  it('refuses to take a non-object argument', function() {
+    expect(function() { new Conc([]) }).toThrow();
+  });
+  it('executes a single function', function() {
+    var conc = new Conc({
+      plus5: new Fun(function(v, next) { next(null, v + 5) })
+    });
+    var wrapped = conc.wrap();
+    var args;
+    wrapped(null, 3, function() {
+      args = Array.prototype.slice.call(arguments, 0);
+    });
+    expect(args.length).toEqual(2);
+    expect(args).toEqual([null, 8]);
+  });
+});
