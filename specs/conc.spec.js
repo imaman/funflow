74d0293001a6acc084d448b9e2c759480b8f346b
Itay Maman <imaman@google.com>
2014-03-24 07:18:49 +0200

    support var args

--- specs/conc.spec.js
+++ specs/conc.spec.js
@@ -34,4 +34,17 @@ describe('Conc', function() {
     expect(args.length).toEqual(2);
     expect(args).toEqual([null, { plus5: 8, plus7: 10, plus9: 12 }]);
   });
+  it('support var args', function() {
+    var conc = new Conc({
+      sum: new Fun(function(v1, v2, next) { next(null, v1 + v2) }),
+      diff: new Fun(function(v1, v2, next) { next(null, v1 - v2) }),
+    });
+    var wrapped = conc.wrap();
+    var args;
+    wrapped(null, 16, 4, function() {
+      args = Array.prototype.slice.call(arguments, 0);
+    });
+    expect(args.length).toEqual(2);
+    expect(args).toEqual([null, { sum: 20, diff: 12 }]);
+  });
 });
