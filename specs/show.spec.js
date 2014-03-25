b257edafb5a5c74f32ecd258f10ad6407f6371e1
Itay Maman <imaman@google.com>
2014-03-25 08:55:08 +0200

    fake it

--- specs/show.spec.js
+++ specs/show.spec.js
@@ -13,5 +13,21 @@ describe('visualization', function() {
       expect(f.show(5)).toEqual('     plusOne()');
     });
   });
+  describe('of a map', function() {
+    it('is single line adjusted by one when map has a single function', function() {
+      var f = new Conc({
+        a: new Fun(function plusOne(v, next) { next(null, v+1) })
+      });
+      expect(f.show().split('\n')).toEqual([
+        '+----+',
+        '     plusOne()',
+        '+----+'
+      ]);
+    });
+    it('is adjusted by the given offset', function() {
+      var f = new Fun(function plusOne(v, next) { next(null, v+1) });
+      expect(f.show(5)).toEqual('     plusOne()');
+    });
+  });
 });
 
