310e4483066b60d0f6d154f5b080e108491c5b49
Itay Maman <imaman@google.com>
2014-04-07 09:08:52 +0300

    more tests

--- specs/spike.spec.js
+++ specs/spike.spec.js
@@ -314,21 +314,87 @@ describe('ASCII diagram', function() {
       var input = ['a', {b1: 'B1', b2: 'B2'}, 'c'];
       var g = treeFromDsl(input, 't');
       verify(g.edges(), [
+        'a -> t0',
         't0 -> B1',
         't0 -> B2',
-        'a -> t0',
         't0 -> c' ]);
     });
     it('map of arrays', function() {
       var input = ['a', {b1: ['B11', 'B12'], b2: ['B21', 'B22']}, 'c'];
       var g = treeFromDsl(input, 't');
       verify(g.edges(), [
-        'B11 -> B12',
-        't0 -> B11',
-        'B21 -> B22',
-        't0 -> B21',
         'a -> t0',
-        't0 -> c']);
+        't0 -> B11',
+        't0 -> B21',
+        't0 -> c',
+        'B11 -> B12',
+        'B21 -> B22']);
+    });
+    it('map of arrays of different lengths', function() {
+      var input = ['a', {b1: ['B11', 'B12', 'B13'], b2: ['B21', 'B22', 'B23', 'B24'], b3: ['B31']}, 'c'];
+      var g = treeFromDsl(input, 't');
+      verify(g.edges(), [
+        'a -> t0',
+        't0 -> B11',
+        't0 -> B21',
+        't0 -> B31',
+        't0 -> c',
+        'B11 -> B12',
+        'B12 -> B13',
+        'B21 -> B22',
+        'B22 -> B23',
+        'B23 -> B24']);
+    });
+    it('map of arrays of maps', function() {
+      var input = ['a', {
+        b1: [
+          { b111: 'B111', b112: 'B112' },
+          { b121: 'B121', b122: 'B122', b123: 'B123' }
+        ],
+        b2: [
+          { b211: 'B211', b212: 'B212' },
+          { b221: 'B221', b222: 'B222', b223: 'B223' },
+          { b231: 'B231', b232: 'B232' }
+        ]},
+        'c',
+        'd'];
+      var g = treeFromDsl(input, 't');
+      verify(g.edges().filter(function(e) { return e.type === 'next' }), [
+        'a -> t0',
+        't0 -> c',
+        'c -> d',
+        't2 -> t1',
+        't5 -> t4',
+        't4 -> t3']);
+
+      verify(g.edges(), [
+        'a -> t0',
+
+        't0 -> t2',
+        't0 -> t5',
+        't0 -> c',
+
+        't2 -> B111',
+        't2 -> B112',
+        't2 -> t1',
+
+        't5 -> B211',
+        't5 -> B212',
+        't5 -> t4',
+
+        't1 -> B121',
+        't1 -> B122',
+        't1 -> B123',
+
+        't4 -> B221',
+        't4 -> B222',
+        't4 -> B223',
+        't4 -> t3',
+
+        't3 -> B231',
+        't3 -> B232',
+
+        'c -> d']);
     });
     it('tags edges in a sequence with "next"', function() {
       var input = ['a', {b1: 'B1', b2: 'B2'}, 'c'];
