b62ac7698509a4be894f1ba4a4fa49089b1b57d3
Itay Maman <imaman@google.com>
2014-04-13 21:49:05 +0300

    move width(), order() out of the specs

--- specs/spike.spec.js
+++ specs/spike.spec.js
@@ -1,7 +1,8 @@
 var Graph = require('../lib/graph');
 var dagFromTree = require('../lib/visualization').dagFromTree;
 var treeFromDsl = require('../lib/visualization').treeFromDsl;
-
+var width = require('../lib/visualization').width;
+var order = require('../lib/visualization').order;
 
 describe('tree/dag representation', function() {
   describe('tree to dag', function() {
@@ -216,48 +217,6 @@ describe('tree/dag representation', function() {
       expect(g.vertex('c').incoming().map(function(e) { return e.type })).toEqual(['next']);
     });
   });
-  function order(v) {
-    var temp = [];
-    var q = [];
-    q.push({v: v, depth: 0});
-    var visitCountByVertex = {};
-
-    while(q.length > 0) {
-      var current = q.shift();
-      var visitCount = visitCountByVertex[current.v] || 0;
-      visitCount += 1;
-      visitCountByVertex[current.v] = visitCount;
-      if (visitCount < current.v.incoming().length) {
-        continue;
-      }
-
-      temp.push(current);
-      current.v.targets().forEach(function(t) {
-        q.push({v: t, depth: current.depth + 1});
-      });
-    }
-
-    function groupBy(array, map) {
-      var groups = {};
-      array.forEach(function(o) {
-        var group = JSON.stringify(map(o));
-        groups[group] = groups[group] || [];
-        groups[group].push(o);
-      });
-      return groups;
-    }
-
-    var groups = groupBy(temp, function(o) { return o.depth });
-    var result = Object.keys(groups).map(function(k) {
-      return {d: k, vs: groups[k].map(function(x) { return x.v })}
-    });
-    result.sort(function(x, y) {
-      var dx = Number(x.d);
-      var dy = Number(y.d);
-      return (dx === dy) ? 0 : (dx < dy) ? -1 : 1;
-    });
-    return result.map(function(x) { return x.vs.map(function(v) { return v.toString() }) });
-  }
   describe('dag to ASCII diagram', function() {
     describe('printing order', function() {
       it('is linear in a sequence', function() {
@@ -307,42 +266,6 @@ describe('tree/dag representation', function() {
       });
     });
     describe('width', function() {
-      function preOrder(root, f) {
-        function dfs(v) {
-          var outgoing = v.outgoing();
-          if (outgoing.length === 0) {
-            return f([], undefined);
-          }
-          if (outgoing.length === 1) {
-            return f([], dfs(outgoing[0].to));
-          }
-
-          var acc = [];
-          var next;
-          outgoing.forEach(function(e) {
-            if (e.type === 'next')
-              next = dfs(e.to);
-            else
-              acc.push(dfs(e.to));
-          });
-          return f(acc, next);
-        }
-
-        return dfs(root);
-      }
-
-      function width(v) {
-        return preOrder(v, function(kids, next) {
-          if (kids.length === 0 && next === undefined)
-            return 1;
-
-          if (kids.length === 0)
-            return next;
-
-          var widthOfKids = kids.reduce(function(a,b) { return a + b }, 1);
-          return Math.max(widthOfKids, next);
-        });
-      }
       it('of a seq. is 1', function() {
         var g = Graph.new_();
         g.connect('a', 'b');
