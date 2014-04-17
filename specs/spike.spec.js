9b51d963b8d8cc6457d81d1428515ac0f54913e4
Itay Maman <imaman@google.com>
2014-04-17 11:07:19 +0300

    fix dim. calculation for conc. branches

--- specs/spike.spec.js
+++ specs/spike.spec.js
@@ -8,25 +8,23 @@ describe('tree representation', function() {
       function preOrder(v, screen) {
         screen.putAt(0, 0, v.key);
 
-        var row = 0;
-        var col = 0;
-        if (v.type !== 'conc') {
+        var row = 1;
+        var col = 1;
+        if (v.type === 'conc') {
           v.targets().forEach(function(t) {
-            ++row;
+            var dim = preOrder(t, screen.nested(1, col));
+            col += dim.p;
+            row = Math.max(row, dim.s);
+          });
+        } else {
+          v.targets().forEach(function(t) {
             var dim = preOrder(t, screen.nested(row, 1));
             row += dim.s;
             col = Math.max(col, dim.p);
           });
-
-          return { s: row, p: col };
+          if (v.targets().length)
+            ++col;
         }
-
-        v.targets().forEach(function(t) {
-          ++col;
-          var dim = preOrder(t, screen.nested(1, col));
-          col += dim.p;
-          row = Math.max(row, dim.s);
-        });
 
         return { s: row, p: col };
       }
@@ -93,6 +91,108 @@ describe('tree representation', function() {
         '   a',
         '   r1',
         '      b1 b2']);
+    });
+    it('two-level concurrency', function() {
+      var g = Graph.new_();
+      g.connect('r0', 'a');
+      g.connect('r0', 'r1');
+      g.connect('r1', 'r2').from.type = 'conc';
+      g.connect('r1', 'b3');
+      g.connect('r2', 'b1').from.type = 'conc';
+      g.connect('r2', 'b2');
+
+      expect(dump(g.vertex('r0')).split('\n')).toEqual([
+        'r0',
+        '   a',
+        '   r1',
+        '      r2       b3',
+        '         b1 b2'
+      ]);
+    });
+    it('concurrency at root', function() {
+      var g = Graph.new_();
+      g.connect('r0', 'a').from.type = 'conc';
+      g.connect('r0', 'b');
+
+      expect(dump(g.vertex('r0'))).toEqual([
+        'r0',
+        '   a b'
+      ].join('\n'));
+    });
+    it('concurrency with inner sequence', function() {
+      var g = Graph.new_();
+      g.connect('r0', 'r1').from.type = 'conc';
+
+      g.connect('r1', 'b1');
+      g.connect('r1', 'b2');
+
+      expect('\n' + dump(g.vertex('r0'))).toEqual(['',
+        'r0',
+        '   r1',
+        '      b1',
+        '      b2'
+      ].join('\n'));
+    });
+    it('sequence inside a concurrent branch', function() {
+      var g = Graph.new_();
+      g.connect('r0', 'r1').from.type = 'conc';
+      g.connect('r0', 'b2');
+
+      g.connect('r1', 'b1');
+
+      expect('\n' + dump(g.vertex('r0'))).toEqual(['',
+        'r0',
+        '   r1    b2',
+        '      b1'
+      ].join('\n'));
+    });
+    xit('two concurrent sequences', function() {
+      var g = Graph.new_();
+      g.connect('r0', 'r1').from.type = 'conc';
+      g.connect('r0', 'r2');
+
+      g.connect('r1', 'b1');
+      g.connect('r1', 'b2');
+
+      g.connect('r2', 'b3');
+      g.connect('r2', 'b4');
+      g.connect('r2', 'b5');
+
+      expect(dump(g.vertex('r0'))).toEqual([
+        'r0',
+        '   r1    r2',
+        '      b1    b3',
+        '      b2    b4',
+        '            b5'
+      ].join('\n'));
+    });
+    xit('two level concurrency with nested sequence', function() {
+      var g = Graph.new_();
+      g.connect('r0', 'a');
+      g.connect('r0', 'r1');
+
+      g.connect('r1', 'r2').from.type = 'conc';
+      g.connect('r1', 'r3');
+      g.connect('r2', 'r4').from.type = 'conc';
+      g.connect('r2', 'b1');
+
+      g.connect('r4', 'b3');
+      g.connect('r4', 'b4');
+      g.connect('r4', 'b5');
+
+      g.connect('r3', 'b6');
+      g.connect('r3', 'b7');
+
+      expect(dump(g.vertex('r0'))).toEqual([
+        'r0',
+        '   a',
+        '   r1',
+        '      r2         r3',
+        '         r4   b1    b6',
+        '            b3      b7',
+        '            b4',
+        '            b5'
+      ].join('\n'));
     });
   });
 });
