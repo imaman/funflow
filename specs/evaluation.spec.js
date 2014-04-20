1f4feaa3e61680db335d5c0220e5d6ce26f7283c
Itay Maman <imaman@google.com>
2014-04-21 01:09:18 +0300

    compilation - fake it

---
+++ specs/evaluation.spec.js
@@ -0,0 +1,23 @@
+var Graph = require('../lib/graph');
+var rootFromDsl = require('../lib/visualization').rootFromDsl;
+var show = require('../lib/visualization').show;
+var u_ = require('underscore');
+
+describe('funflow compilation', function() {
+  function compile(v) {
+    return function(e, a1, a2, next) { next(null, 10) };
+  }
+  it('transforms a single function into a fully-async one', function() {
+    var root = rootFromDsl(
+      function sum(x, y, next) { next(null, x + y) }
+    );
+    var flow = compile(root);
+    var args;
+    flow(null, 3, 7, function(e, v) {
+      args = [e, v];
+    });
+    expect(args).toEqual([null, 10]);
+  });
+});
+
+
