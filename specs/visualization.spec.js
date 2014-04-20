956c097cc0d83e64985ded68074fba7fc981c3d5
Itay Maman <imaman@google.com>
2014-04-20 08:24:46 +0300

    SaffSequeeze - step 12: refine the assertion

--- specs/visualization.spec.js
+++ specs/visualization.spec.js
@@ -576,7 +576,7 @@ describe('tree/dag representation', function() {
             var widths = u_.range(0, max.c + 1).map(u_.constant(1));
 
             this.entries.filter(function(current) { return current.c === 0}).forEach(function(current) {
-              expect(current.v.length).not.toEqual(7) || expect(current.v).toEqual('??');
+              expect(current.v).not.toEqual('_temp_0');
             });
           }
 
