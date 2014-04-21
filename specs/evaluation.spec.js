9831d2873522ea613b0855653910cd79aaacbd70
Itay Maman <imaman@google.com>
2014-04-21 10:46:12 +0300

    rename test

--- specs/evaluation.spec.js
+++ specs/evaluation.spec.js
@@ -7,16 +7,18 @@ describe('funflow compilation', function() {
   function compile(v) {
     return function(e, a1, a2, next) { v.payload(a1, a2, next) };
   }
-  it('transforms a single function into a fully-async one', function() {
-    var root = rootFromDsl(
-      function sum(x, y, next) { next(null, x + y) }
-    );
-    var flow = compile(root);
-    var args;
-    flow(null, 3, 7, function(e, v) {
-      args = [e, v];
+  describe('of a function', function() {
+    it('turns an (arg, ..., next) function into an (e, arg, ..., next) callback', function() {
+      var root = rootFromDsl(
+        function sum(x, y, next) { next(null, x + y) }
+      );
+      var flow = compile(root);
+      var args;
+      flow(null, 3, 7, function(e, v) {
+        args = [e, v];
+      });
+      expect(args).toEqual([null, 10]);
     });
-    expect(args).toEqual([null, 10]);
   });
 });
 
