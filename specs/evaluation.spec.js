7042f11d7e714ed2214c7a075cadde4df3a21cb0
Itay Maman <imaman@google.com>
2014-04-21 20:44:15 +0300

    move compile() to lib/evaluation.js

--- specs/evaluation.spec.js
+++ specs/evaluation.spec.js
@@ -1,7 +1,6 @@
-var Graph = require('../lib/graph');
 var rootFromDsl = require('../lib/dsl').rootFromDsl;
-var show = require('../lib/visualization').show;
 var u_ = require('underscore');
+var compile = require('../lib/evaluation').compile;
 
 describe('funflow compilation', function() {
   describe('of a function', function() {
@@ -270,77 +269,6 @@ describe('funflow compilation', function() {
       expect(args[0]).toBe(error);
     });
   });
-  function compile(v) {
-    var compiled = v.targets().map(compile);
-    if (v.type === 'conc') {
-      var edges = v.outgoing();
-      return function() {
-        var args = u_.toArray(arguments);
-        var next = args.pop();
-
-        var countdown = edges.length;
-        var result = {};
-        function merge(k, e) {
-          if (countdown === 0)
-            return;
-
-          if (e) {
-            countdown = 0;
-            return next(e);
-          }
-          result[k] = u_.toArray(arguments).slice(2);
-          --countdown;
-          if (countdown === 0)
-            next(null, result);
-        }
-        var zipped = u_.zip(edges.map(function(e) { return e.name }), compiled);
-        zipped.forEach(function(current) {
-          var key = current[0];
-          var func = current[1];
-
-          var bounded = merge.bind(null, key);
-          func.apply(null, args.concat(bounded));
-        });
-      }
-    }
-    if (compiled.length > 0) {
-      return function() {
-        var args = u_.toArray(arguments);
-        var next = args.pop();
-
-        var reduced = compiled.reduceRight(function(soFar, current) {
-          return function() {
-            var args = u_.toArray(arguments);
-            args.push(soFar);
-            return current.apply(null, args);
-          }
-        }, next);
-
-        reduced.apply(null, args);
-      }
-    }
-    return function() {
-      var args = u_.toArray(arguments);
-      if (args.length === 0)
-        throw new Error('No error argument was passed in');
-      if (args.length === 1)
-        throw new Error('No next() argument was passed in');
-
-      var numPassed = args.length - 2;
-      var func = v.payload;
-      var numExpected = func.length - 1;
-      if (numExpected !== numPassed)
-        throw new Error(v.payload.name + '() expects ' + numExpected + ' arguments but ' + numPassed + ' were passed');
-      var e = args[0];
-      var trap = u_.last(args);
-      if (e) return trap(e);
-      try {
-        v.payload.apply(null, args.slice(1));
-      } catch (exception) {
-        trap(exception);
-      }
-    }
-  }
 });
 
 
