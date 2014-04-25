8ef4d13d029779fc48c5ce00a7f4d120c6a3ae04
Itay Maman <imaman@google.com>
2014-04-25 17:14:11 +0300

    timers

--- specs/evaluation.spec.js
+++ specs/evaluation.spec.js
@@ -2,6 +2,7 @@ var rootFromDsl = require('../lib/dsl').rootFromDsl;
 var comp = require('../lib/dsl').comp;
 var single = require('../lib/dsl').single;
 var fork = require('../lib/dsl').fork;
+var timer = require('../lib/dsl').timer;
 var u_ = require('underscore');
 var compile = require('../lib/evaluation').compile;
 
@@ -445,6 +446,79 @@ describe('funflow compilation', function() {
 
     flow(null, 7, function() { args = u_.toArray(arguments) });
     expect(args).toEqual([null, '17,70']);
+  });
+  describe('timers', function() {
+    it('fires a result for its slot', function(done) {
+      var flow = compile(rootFromDsl(fork({
+        a: single(function(v, next) { next(null, v + 'A') }),
+        b: function(v, next) {},
+        elapsed: timer(1),
+      }, function(result, next) {
+        if (result.a && result.elapsed > 10)
+          next(null, result);
+      })));
+
+      flow(null, '**', function(e, v) {
+        expect(arguments.length).toEqual(2);
+        expect(e).toBe(null);
+        expect(v.a).toEqual('**A');
+        expect(v.b).toBe(undefined);
+        expect(v.elapsed).toBeGreaterThan(10);
+
+        done();
+      });
+    });
+    it('fires several times, each time reporting the elapsed time', function(done) {
+      var acc = [];
+      var flow = compile(rootFromDsl(fork({
+        elapsed: timer(1)
+      }, function(result, next) {
+        acc.push(result.elapsed);
+        if (result.elapsed >= 20)
+          next(null, result);
+      })));
+
+      flow(null, '**', function() {
+        // a 1-ms frequency for 20 ms, should (conservatively) fire at least
+        // three times.
+        expect(acc.length).toBeGreaterThan(3);
+        acc.reduce(function(prev, curr) {
+          expect(curr).toBeGreaterThan(prev);
+          return curr;
+        }, -1);
+        done();
+      });
+    });
+    it('fires an excpetion after the specified timeout duration', function(done) {
+      var acc = [];
+      var flow = compile(rootFromDsl(fork({
+        elapsed: timer(1, 10)
+      }, function(result, next) {
+        acc.push(result.elapsed);
+      })));
+
+      flow(null, function(e) {
+        expect(arguments.length).toEqual(1);
+        expect(e.message).toEqual('Timeout');
+        expect(e.duration).toBeGreaterThan(10);
+        done();
+      });
+    });
+    it('can be used inside a seq', function(done) {
+      var acc = [];
+      var flow = compile(rootFromDsl([
+        timer(15),
+        function (millis, next) {
+          next(null, millis / 1000.0)
+        }]));
+
+      flow(null, function(e, v) {
+        expect(arguments.length).toEqual(2);
+        expect(e).toBe(null);
+        expect(v).toBeCloseTo(0.015, 1);
+        done();
+      });
+    });
   });
 });
 
