636393c916dcce94e714f95541cd1aa7aa00afb2
Itay Maman <imaman@google.com>
2014-04-27 19:40:29 +0300

    seaprate evaluation

--- specs/evaluation.spec.js
+++ specs/evaluation.spec.js
@@ -4,7 +4,7 @@ var single = require('../lib/dsl').single;
 var fork = require('../lib/dsl').fork;
 var timer = require('../lib/dsl').timer;
 var newFlow = require('../lib/evaluation').newFlow;
-var prepare = require('../lib/evaluation').prepare;
+var prepare = require('../lib/evaluation2').prepare;
 
 describe('funflow compilation', function() {
   describe('of a literal', function() {
