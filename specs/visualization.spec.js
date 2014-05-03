a126caf3a23a3dd4f6251a32dc632a407f4b6bc1
Itay Maman <imaman@google.com>
2014-05-03 15:02:06 +0300

    show IDs of computations in the diagram

--- specs/visualization.spec.js
+++ specs/visualization.spec.js
@@ -155,24 +155,38 @@ describe('visualization', function() {
     it('shows IDs', function() {
       var executable = compile([
         'A',
+        'B'
+      ]);
+      expect('\n' + executable.toString()).toEqual(['',
+        '',
+        '|',
+        'A#0',
+        '|',
+        'B#1',
+        '|'
+      ].join('\n'));
+    });
+    it('shows IDs in a complex graph', function() {
+      var executable = compile([
+        'A',
         {b1: 'B1', b2: ['B21', 'B22']},
         'C']);
       expect('\n' + executable.toString()).toEqual(['',
         '',
         '|',
-        'A',
+        'A#0',
         '|',
         '|',
-        '+->-+--+',
-        '    |  |',
-        '    B1 |',
-        '    |  B21',
-        '    |  |',
-        '    |  B22',
-        '    v  |',
-        '    |  |',
-        '+-<-+--+',
-        'C',
+        '+->-+----+',
+        '    |    |',
+        '    B1#3 |',
+        '    |    B21#4',
+        '    |    |',
+        '    |    B22#5',
+        '    v    |',
+        '    |    |',
+        '+-<-+----+',
+        'C#6',
         '|'
       ].join('\n'));
     });
