7b45e19805ed3c35d69862e0f40c0fb480515b5d
Itay Maman <imaman@google.com>
2014-05-03 20:27:32 +0300

    Treat var args as a seq

--- specs/compilation.spec.js
+++ specs/compilation.spec.js
@@ -1,6 +1,30 @@
 var compile = require('../lib/compilation').compile;
+var Compiler = require('../lib/compilation').Compiler;
 
 describe('compilation', function() {
+  describe('translation of DSL into Flow', function() {
+    it('creates a flow when .compile() is called', function() {
+      var flow = Compiler.new_().compile(['A', 'B']);
+      expect('\n' + flow.toString()).toEqual(['',
+        '',
+        '|',
+        'A#0',
+        '|',
+        'B#1',
+        '|'
+      ].join('\n'));
+    });
+    it('treats var args as a sequence', function() {
+      var seqFlow = Compiler.new_().compile(['A', 'B']);
+      var varArgflow = Compiler.new_().compile('A', 'B');
+      expect(varArgflow.toString()).toEqual(seqFlow.toString());
+    });
+    it('treats var args as a sequence when the compile() function is used', function() {
+      var seqFlow = compile(['A', 'B']);
+      var varArgflow = compile('A', 'B');
+      expect(varArgflow.toString()).toEqual(seqFlow.toString());
+    });
+  });
   describe('diagram of a fully compiled flow', function() {
     it('shows IDs', function() {
       var executable = compile([
