8b8f8f6bd6bdf91c332c9f0bc56ce5a7d38e20b2
Itay Maman <imaman@google.com>
2014-05-04 18:15:15 +0300

    test newExecution

--- specs/flow.spec.js
+++ specs/flow.spec.js
@@ -3,14 +3,14 @@ var Compiler = require('../lib/compilation').Compiler;
 
 describe('flow', function() {
   describe('execution', function() {
-    xit('creates an Execution', function() {
+    it('creates an Execution', function() {
       var flow = Compiler.new_().compile('A', 'B');
-      var execution = flow.newExeuction();
-      execution.run(function(e, v) {
+      var execution = flow.newExecution();
+      execution.run(null, function(e, v) {
         expect(e).toBe(null);
         expect(v).toEqual('B');
-        exepct(execution.outputOf(0)).toEqual('A');
-        exepct(execution.outputOf(1)).toEqual('B');
+        expect(execution.outputOf(0)).toEqual([null, 'A']);
+        expect(execution.outputOf(1)).toEqual([null, 'B']);
       });
     });
   });
